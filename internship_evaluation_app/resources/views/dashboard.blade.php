bff28aa00aa9342da3c04fa5f042f546c9b14fb0
Kevin98 <kevinjespers98@gmail.com>
2023-03-30 10:15:01 +0200

    dashboard add name of user

--- internship_evaluation_app/resources/views/dashboard.blade.php
+++ internship_evaluation_app/resources/views/dashboard.blade.php
@@ -9,11 +9,14 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
-
-                    {{ auth()->user(); }}
+                    @php
+                        $user = auth()->user();
+                    @endphp
+                    {{ __("You're logged in!") }}<br>
+                    {{ __("Welcome, ".$user->name)}}
                 </div>
             </div>
         </div>
     </div>
+
 </x-app-layout>
