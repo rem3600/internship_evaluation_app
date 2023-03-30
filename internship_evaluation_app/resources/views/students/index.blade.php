9eca0a2022b24c6174c755ec6dcff99042272e7c
Remco Duckers <rempel.duckers@telenet.be>
2023-03-30 11:59:38 +0200

    added styling to h2 element

--- internship_evaluation_app/resources/views/students/index.blade.php
+++ internship_evaluation_app/resources/views/students/index.blade.php
@@ -10,7 +10,7 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
                     {{ __("You're logged in,") }} {{ Auth::user()->name }}!
-                    <h2>Students</h2>
+                    <h2 class="text-xl pt-6">Students</h2>
                 </div>
             </div>
         </div>
