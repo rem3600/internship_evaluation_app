4806da8014a690db4bb2362ffece620c226d20c0
Kevin98 <kevinjespers98@gmail.com>
2023-04-03 14:07:01 +0200

    hash password

--- internship_evaluation_app/app/Http/Controllers/UserTableController.php
+++ internship_evaluation_app/app/Http/Controllers/UserTableController.php
@@ -7,6 +7,9 @@ use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\View\View;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Password;
+use Illuminate\Validation\Rules;
 
 
 class UserTableController extends Controller
@@ -41,13 +44,14 @@ class UserTableController extends Controller
         $data = $request->validate([
             'name' => 'required|string|max:100',
             'email' => 'required|string|max:100',
-            'password' => 'required|string|max:100',
+            'password' => ['required', Rules\Password::defaults()]
+            // 'password' => Hash::make($request->password)
         ]);
 
         $user = new User();
         $user->name = $data['name'];
         $user->email = $data['email'];
-        $user->password = $data['password'];
+        $user->password = Hash::make($data['password']);
         $user->save();
 
         return redirect('dashboard')->with('success', 'user added successfully');
@@ -58,41 +62,57 @@ class UserTableController extends Controller
      */
     public function show(User $users)
     {
-        // $users = User::whereHas('role_id', '0')->get();
-                                
-        // return view('dashboard', ['user' => $user]);
+        
 
-        // $role_id = Auth::role_id();
-        $users = User::All();
-                    // where('role_id', $role_id)
-                    //         ->latest()
-                    //         ->get();
+        // get all teachers (role_id = NULL) when an admin logs in
+
+        $id = Auth::User()->role_id;
+
+        
+         if ($id == 3) {
+            $users = User::All()->where('role_id', NULL);
+            return view('dashboard', ['users' => $users]); 
+         }
+
+         
+
         
         
-        return view('dashboard', ['users' => $users]);
+        
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(UserTable $userTable)
+    public function edit(User $user)
     {
-        //
+        
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, UserTable $userTable)
+    public function update(Request $request, User $user)
     {
-        //
+        $data = $request->validate([
+            'name' => 'required|string|max:100',
+            'email' => 'required|string|max:100',
+            'password' => 'required|string|max:100',
+            'password' => Hash::make($request->password),
+        ]);
+
+        $user->update($data);
+
+        return redirect('dashboard');
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(UserTable $userTable)
+    public function destroy(User $user)
     {
-        //
+        $user->delete();
+
+        return redirect('dashboard');
     }
 }
